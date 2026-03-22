30597bf829ec867dbc9c7f7ec42ab8c8c321bb97
TotallyNotBiased <highfunctioningpsychopath@gmail.com>
2026-03-22 18:52:57 +1100

    fixed: neotree syntax error

--- modules/home/nixvim/plugins/neo-tree.nix
+++ modules/home/nixvim/plugins/neo-tree.nix
@@ -1,7 +1,7 @@
 {  
   programs.nixvim.plugins.neo-tree = {
     enable = true;
-    close_if_last_window = true; 
+    closeIfLastWindow = true; 
     window = {
       settings.width = 30;
       auto_expand_width = true;
