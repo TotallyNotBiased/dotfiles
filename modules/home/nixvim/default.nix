78c99693e73adcec8ffe088dab829dfbbe2bb293
TotallyNotBiased <highfunctioningpsychopath@gmail.com>
2026-03-23 16:44:03 +1100

    feature: custom theming, using vim's gotham

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -15,7 +15,27 @@
       defaultEditor = true;
       vimAlias = true;
 
-      colorschemes.catppuccin.enable = true;
+      colorschemes.base16 = {
+        enable = true;
+        colorscheme = {
+          base00 = "${config.theme.colors.base00}";
+          base01 = "${config.theme.colors.base01}";
+          base02 = "${config.theme.colors.base02}";
+          base03 = "${config.theme.colors.base03}";
+          base04 = "${config.theme.colors.base04}";
+          base05 = "${config.theme.colors.base05}";
+          base06 = "${config.theme.colors.base06}";
+          base07 = "${config.theme.colors.base07}";
+          base08 = "${config.theme.colors.red}";
+          base09 = "${config.theme.colors.orange}";
+          base0A = "${config.theme.colors.yellow}";
+          base0B = "${config.theme.colors.green}";
+          base0C = "${config.theme.colors.cyan}";
+          base0D = "${config.theme.colors.blue}";
+          base0E = "${config.theme.colors.magenta}";
+          base0F = "${config.theme.colors.violet}";
+        };
+      };
       web-devicons.enable = true;
       version.enableNixpkgsReleaseCheck = false;
   };
---
+++ modules/home/theme.nix
@@ -0,0 +1,25 @@
+{ config, lib, ... }:
+
+{
+  options.theme = {
+    colors = {
+      base00 = lib.mkOption { type = lib.types.str; default = "#0c1014"; };
+      base01 = lib.mkOption { type = lib.types.str; default = "#11151c"; };
+      base02 = lib.mkOption { type = lib.types.str; default = "#091f2e"; };
+      base03 = lib.mkOption { type = lib.types.str; default = "#0a3749"; };
+      base04 = lib.mkOption { type = lib.types.str; default = "#1e6479"; };
+      base05 = lib.mkOption { type = lib.types.str; default = "#599cab"; };
+      base06 = lib.mkOption { type = lib.types.str; default = "#99d1ce"; };
+      base07 = lib.mkOption { type = lib.types.str; default = "#d3ebe9"; };
+
+      red     = lib.mkOption { type = lib.types.str; default = "#c23127"; };
+      orange  = lib.mkOption { type = lib.types.str; default = "#d26937"; };
+      yellow  = lib.mkOption { type = lib.types.str; default = "#edb443"; };
+      green   = lib.mkOption { type = lib.types.str; default = "#2aa889"; };
+      cyan    = lib.mkOption { type = lib.types.str; default = "#33859E"; };
+      blue    = lib.mkOption { type = lib.types.str; default = "#195466"; };
+      magenta = lib.mkOption { type = lib.types.str; default = "#888ca6"; };
+      violet  = lib.mkOption { type = lib.types.str; default = "#4e5166"; };
+    };
+  };
+}
