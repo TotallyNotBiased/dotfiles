b5879b7896294ecf2680e8f9866bab951db48506
TotallyNotBiased <highfunctioningpsychopath@gmail.com>
2026-03-23 16:37:07 +1100

    refactored: home-manager config to mkOutOfStoreSymlink

--- hosts/thinkpad/home.nix
+++ hosts/thinkpad/home.nix
@@ -27,7 +27,6 @@
         qalculate-gtk
         wl-clipboard
         cliphist
-        swaylock
         glib
         gsettings-desktop-schemas
       ])
@@ -81,48 +80,7 @@
       rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles#nixos";
       conf = "cd ~/dotfiles";
     };
-    profileExtra = ''
-        if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-          exec niri
-        fi
-      # '';
   };
 
-  programs.fuzzel = {
-    enable = true;
-    settings = {
-      main = {
-        font = "JetBrains Mono:size=11";
-        icon-theme = "Papirus-Dark";
-
-        width = 45;
-        lines = 10;
-        "line-height" = 25;
-        "horizontal-pad" = 25;
-        "vertical-pad" = 25;
-        "inner-pad" = 10;
-
-        prompt = "'❯   '";
-        layer = "top";
-      };
-
-      colors = {
-        # rrggbbaa
-        background = "1e1e2edd"; # purple-grey with alpha
-        text = "cdd6f4ff"; # off-white blue
-        match = "f5c2e7ff"; # pink
-
-        selection = "cba6f7ff"; # mauve accent
-        "selection-text" = "1e1e2eff"; # dark text on selection
-        "selection-match" = "1e1e2eff";
-
-        border = "cba6f7ff"; # mauve border
-      };
-
-      border = {
-        width = 2;
-        radius = 10;
-      };
-    };
-  };
+  xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/niri/config.kdl";
 }
--- hosts/thinkpad/noctalia.nix
+++ hosts/thinkpad/noctalia.nix
@@ -1,69 +1,14 @@
 { pkgs, inputs, ... }:
 {
-  home-manager.users.unbiased = {
+  home-manager.users.unbiased = { config, ... }: {
     imports = [
       inputs.noctalia.homeModules.default
     ];
 
     programs.noctalia-shell = {
       enable = true;
-      settings = {
-        bar = {
-          density = "compact";
-          position = "top";
-          showCapsule = false;
-          widgets = {
-            left = [
-              {
-                id = "ControlCenter";
-                useDistroLogo = true;
-              }
-              {
-                id = "WiFi";
-              }
-              {
-                id = "Bluetooth";
-              }
-            ];
-            center = [
-              {
-                hideUnoccupied = false;
-                id = "Workspace";
-                labelMode = "none";
-              }
-            ];
-            right = [
-              {
-                alwaysShowPercentage = false;
-                id = "Battery";
-                warningThreshold = 30;
-              }
-              {
-                formatHorizontal = "HH:mm";
-                formatVertical = "HH mm";
-                id = "Clock";
-                useMonospacedFont = true;
-                usePrimaryColor = true;
-              }
-            ];
-          };
-        };
-        colorSchemes.predefinedScheme = "Monochrome";
-        general = {
-          avatarImage = "/home/unbiased/.face";
-          radiusRatio = 0.2;
-        };
-        location = {
-          monthBeforeDay = false;
-          name = "Melbourne, Australia";
-        };
-        wallpaper = {
-          enabled = true;
-          directory = "/home/unbiased/img/wallpaper/";
-        };
-      };
-      # this may also be a string or a path to a JSON file,
-      # but in this case must include *all* settings.
     };
+
+    xdg.configFile."noctalia/settings.json".source = config.lib.file.mkOutOfStoreSymlink "/home/unbiased/dotfiles/config/noctalia/settings.json";
   };
 }
