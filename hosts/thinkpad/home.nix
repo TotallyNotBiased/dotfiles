a92902bd249560fe44db492bad9bfb242862fedd
TotallyNotBiased <highfunctioningpsychopath@gmail.com>
2026-03-23 16:37:31 +1100

    added cursor

--- hosts/thinkpad/home.nix
+++ hosts/thinkpad/home.nix
@@ -15,6 +15,14 @@
   ];
 
   home = {
+    pointerCursor = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+      size = 24;
+      gtk.enable = true;
+      x11.enable = true;
+    };
+
     packages =
       (with pkgs; [
         discord
