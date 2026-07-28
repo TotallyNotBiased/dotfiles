5eb99ebe1ace0bb311bfba1c8a010a974b05a280
TotallyNotBiased <hi@jerromeyong.com>
2026-07-28 15:04:19 +1000

    update: lockfiles and package inputs for 26.05

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "unified nix config for thinkpad and wsl";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
