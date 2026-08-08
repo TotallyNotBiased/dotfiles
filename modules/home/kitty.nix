a2503c5d2cb223d4074b206625b0a613e7fa6dd1
TotallyNotBiased <hi@jerromeyong.com>
2026-08-08 16:10:39 +1000

    fix: kitty colours

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -8,7 +8,6 @@
       name = "JetBrains Mono Nerd Font";
       size = 11;
     };
-
     settings = {
       confirm_os_window_close = 0;
       background_opacity = "0.75";
@@ -21,17 +20,17 @@
       color0 = config.theme.colors.base00;
       color8 = config.theme.colors.base03;
       color1 = config.theme.colors.red;
-      color9 = config.theme.colors.orange;
+      color9 = config.theme.colors.red;
       color2 = config.theme.colors.green;
-      color10 = config.theme.colors.base01;
+      color10 = config.theme.colors.green;
       color3 = config.theme.colors.yellow;
-      color11 = config.theme.colors.base02;
+      color11 = config.theme.colors.yellow;
       color4 = config.theme.colors.blue;
-      color12 = config.theme.colors.base04;
+      color12 = config.theme.colors.blue;
       color5 = config.theme.colors.magenta;
-      color13 = config.theme.colors.base06;
+      color13 = config.theme.colors.magenta;
       color6 = config.theme.colors.cyan;
-      color14 = config.theme.colors.violet;
+      color14 = config.theme.colors.cyan;
       color7 = config.theme.colors.base05;
       color15 = config.theme.colors.base07;
     };
